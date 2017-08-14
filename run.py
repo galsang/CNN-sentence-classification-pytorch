c55583d27a279aa0a26f78f42f0b8e636329e229
galsang <taeuk@europa.snu.ac.kr>
2017-08-14 20:54:58 +0900

    TREC data dev decomposition / shuffle / early stopping

--- run.py
+++ run.py
@@ -6,16 +6,23 @@ import torch.optim as optim
 import torch.nn as nn
 
 import numpy as np
+from sklearn.utils import shuffle
 
 
-def train(train_x, train_y, params, classes, word_idx, lr=0.1):
+def train(train_x, train_y, dev_x, dev_y, params):
+    word_idx = params["word_idx"]
+    classes = params["classes"]
 
     model = CNN(**params).cuda()
 
-    optimizer = optim.Adadelta(model.parameters(), lr)
+    optimizer = optim.Adadelta(model.parameters(), params["LEARNING_RATE"])
     criterion = nn.CrossEntropyLoss()
 
-    for e in range(params["EPOCH"]):
+    epoch = 0
+    pre_dev_acc = 0
+    while(True):
+        epoch += 1
+
         for i in range(0, len(train_x), params["BATCH_SIZE"]):
             batch_range = min(params["BATCH_SIZE"], len(train_x) - i)
 
@@ -30,13 +37,27 @@ def train(train_x, train_y, params, classes, word_idx, lr=0.1):
             loss.backward()
             optimizer.step()
 
+            # constrain l2-norms of the weight vectors
+            if model.fc.weight.norm() > params["NORM_LIMIT"]:
+                model.fc.weight.data = model.fc.weight.data * params["NORM_LIMIT"] / model.fc.weight.data.norm()
+
+        dev_acc = test(dev_x, dev_y, model, params)
+        print("epoch:", epoch, ", acc:", dev_acc)
+        if dev_acc <= pre_dev_acc:
+            print("early stopping!")
+            break
+        else:
+            pre_dev_acc = dev_acc
 
     return model
 
 
-def test(test_x, test_y, model, params, vocab, classes, word_idx):
-    batch_x = []
+def test(test_x, test_y, model, params):
+    vocab = params["vocab"]
+    word_idx = params["word_idx"]
+    classes = params["classes"]
 
+    batch_x = []
     for sent in test_x:
         sent_idx = []
         for w in sent:
@@ -52,8 +73,10 @@ def test(test_x, test_y, model, params, vocab, classes, word_idx):
     batch_y = [classes.index(c) for c in test_y]
 
     pred = np.argmax(model(batch_x).cpu().data.numpy(), axis=1)
-    acc = sum([1 if p==y else 0 for p, y in zip(pred, batch_y)]) / len(pred)
-    print("acc:", acc)
+    acc = sum([1 if p == y else 0 for p, y in zip(pred, batch_y)]) / len(pred)
+
+    return acc
+
 
 
 def read_data(path):
@@ -64,19 +87,23 @@ def read_data(path):
             y.append(line[:-1].split()[0].split(":")[0])
             x.append(line[:-1].split()[1:])
 
-    return x, y
+    return shuffle(x, y)
 
 
 def main():
-    train_x, train_y = read_data("data/TREC_train.txt")
-    test_x, test_y = read_data("data/TREC_test.txt")
+    train_x, train_y = read_data("data/TREC/TREC_train.txt")
+    dev_idx = len(train_x)//10
+    dev_x, dev_y, train_x, train_y = train_x[:dev_idx], train_y[:dev_idx], train_x[dev_idx:], train_y[dev_idx:]
+    test_x, test_y = read_data("data/TREC/TREC_test.txt")
 
     vocab = sorted(list(set([w for sent in train_x for w in sent])))
     classes = sorted(list(set(train_y)))
     word_idx = {w: i for i, w in enumerate(vocab)}
 
     params = {
-        "EPOCH": 5,
+        "vocab": vocab,
+        "classes": classes,
+        "word_idx": word_idx,
         "BATCH_SIZE": 50,
         "MAX_SENT_LEN": max([len(sent) for sent in train_x]),
         "WORD_DIM": 300,
@@ -85,12 +112,16 @@ def main():
         "CLASS_SIZE": len(classes),
         "FILTERS": [3, 4, 5],
         "FILTER_NUM": [100, 100, 100],
-        "DROPOUT_PROB": 0.5
+        "DROPOUT_PROB": 0.5,
+        "NORM_LIMIT": 3,
+        "LEARNING_RATE": 0.1
     }
 
-    model = train(train_x, train_y, params, classes, word_idx)
-    test(test_x, test_y, model, params, vocab, classes, word_idx)
+    model = train(train_x, train_y, dev_x, dev_y, params)
+    test_acc = test(test_x, test_y, model, params)
+    print("test acc:", test_acc)
 
 
 if __name__ == "__main__":
-    main()
+    for i in range(10):
+        main()
