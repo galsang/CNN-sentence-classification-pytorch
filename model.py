45c9dcf2624a2f17f2eb1590137c2e61e5b688e4
galsang <taeuk@europa.snu.ac.kr>
2017-08-14 18:28:16 +0900

    basic model implemented

--- model.py
+++ model.py
@@ -1,47 +1,47 @@
 import torch
-from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class CNN(nn.Module):
-    def __init__(self):
+    def __init__(self, **kwargs):
         super(CNN, self).__init__()
 
-        self.BATCH_SIZE = 50
-        self.MAX_SENT_LEN = 100
-        self.WORD_DIM = 300
-        self.IN_CHANNEL = 1
-        self.VOCAB_SIZE = 50000
+        self.BATCH_SIZE = kwargs["BATCH_SIZE"]
+        self.MAX_SENT_LEN = kwargs["MAX_SENT_LEN"]
+        self.WORD_DIM = kwargs["WORD_DIM"]
+        self.IN_CHANNEL = kwargs["IN_CHANNEL"]
+        self.VOCAB_SIZE = kwargs["VOCAB_SIZE"]
+        self.CLASS_SIZE = kwargs["CLASS_SIZE"]
+        self.FILTERS = kwargs["FILTERS"]
+        self.FILTER_NUM = kwargs["FILTER_NUM"]
+        self.DROPOUT_PROB = kwargs["DROPOUT_PROB"]
 
-        self.FILTERS = [3, 4, 5]
-        self.FILTER_NUM = [100, 100, 100]
         assert (len(self.FILTERS) == len(self.FILTER_NUM))
 
-        self.convs = [
-            nn.Conv1d(self.IN_CHANNEL, self.FILTER_NUM[i], self.WORD_DIM * self.FILTERS[i], stride=self.WORD_DIM)
-            for i in range(len(self.FILTERS))
-        ]
+        # one for UNK and one for zero padding
+        self.embedding = nn.Embedding(self.VOCAB_SIZE + 2, self.WORD_DIM, padding_idx=self.VOCAB_SIZE + 1)
 
-        self.fc = nn.Linear(sum(self.FILTER_NUM), self.VOCAB_SIZE)
+        for i in range(len(self.FILTERS)):
+            conv = nn.Conv1d(self.IN_CHANNEL, self.FILTER_NUM[i], self.WORD_DIM * self.FILTERS[i], stride=self.WORD_DIM)
+            setattr(self, f'conv_{i}', conv)
 
-    def forward(self):
-        x = Variable(torch.randn(self.BATCH_SIZE, self.IN_CHANNEL, self.WORD_DIM, self.MAX_SENT_LEN))
+        self.fc = nn.Linear(sum(self.FILTER_NUM), self.CLASS_SIZE)
+
+    def get_conv(self, i):
+        return getattr(self, f'conv_{i}')
+
+    def forward(self, x, is_train=False):
+        x = self.embedding(x)
         x = x.view(-1, self.IN_CHANNEL, self.WORD_DIM * self.MAX_SENT_LEN)
 
-        # (N, C_out, MAX_SENT_LEN - FILTER_LEN + 1)
-
         conv_results = [
-            F.max_pool1d(
-                F.relu(self.convs[i](x)),
-                self.MAX_SENT_LEN - self.FILTERS[i] + 1)
+            F.max_pool1d(F.relu(self.get_conv(i)(x)), self.MAX_SENT_LEN - self.FILTERS[i] + 1)
                 .view(-1, self.FILTER_NUM[i])
-            for i in range(len(self.FILTERS))
-        ]
+            for i in range(len(self.FILTERS))]
 
         x = torch.cat(conv_results, 1)
-        x = F.dropout(x, p=0.5, training=False)
+        x = F.dropout(x, p=self.DROPOUT_PROB, training=is_train)
         x = self.fc(x)
 
         return x
-
