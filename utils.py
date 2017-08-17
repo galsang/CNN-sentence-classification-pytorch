5a4c564378cd177ff589be26a4e873bad8b2ae68
galsang <taeuk@europa.snu.ac.kr>
2017-08-18 02:26:35 +0900

    utils.py added

---
+++ utils.py
@@ -0,0 +1,73 @@
+from sklearn.utils import shuffle
+
+import pickle
+
+
+def read_TREC():
+    data = {}
+
+    def read(mode):
+        x, y = [], []
+
+        with open("data/TREC/TREC_" + mode + ".txt", "r", encoding="utf-8") as f:
+            for line in f:
+                y.append(line[:-1].split()[0].split(":")[0])
+                x.append(line[:-1].split()[1:])
+
+        x, y = shuffle(x, y)
+
+        if mode == "train":
+            dev_idx = len(x) // 10
+            data["dev_x"], data["dev_y"] = x[:dev_idx], y[:dev_idx]
+            data["train_x"], data["train_y"] = x[dev_idx:], y[dev_idx:]
+        else:
+            data["test_x"], data["test_y"] = x, y
+
+    read("train")
+    read("test")
+
+    return data
+
+
+def read_MR():
+    data = {}
+    x, y = [], []
+
+    with open("data/MR/rt-polarity.pos", "r", encoding="utf-8") as f:
+        for line in f:
+            x.append(line[:-1].split())
+            y.append(1)
+
+    with open("data/MR/rt-polarity.neg", "r", encoding="utf-8") as f:
+        for line in f:
+            x.append(line[:-1].split())
+            y.append(0)
+
+    x, y = shuffle(x, y)
+    dev_idx = len(x) // 10 * 8
+    test_idx = len(x) // 10 * 9
+
+    data["train_x"], data["train_y"] = x[:dev_idx], y[:dev_idx]
+    data["dev_x"], data["dev_y"] = x[dev_idx:test_idx], y[dev_idx:test_idx]
+    data["test_x"], data["test_y"] = x[test_idx:], y[test_idx:]
+
+    return data
+
+
+def save_model(model, params):
+    path = f"model/{params['DATASET']}_{params['MODEL']}.pkl"
+    pickle.dump(model, open(path, "wb"))
+    print(f"A model is saved successfully as {path}!")
+
+
+def load_model(params):
+    path = f"model/{params['DATASET']}_{params['MODEL']}.pkl"
+
+    try:
+        model = pickle.load(open(path, "rb"))
+        print(f"Model in {path} loaded successfully!")
+
+        return model
+    except:
+        print(f"No available model such as {path}.")
+        exit()
