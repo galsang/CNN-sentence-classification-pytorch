6917a06103d89b0435e22a27e4a3a7d637083d54
galsang <taeuk@europa.snu.ac.kr>
2017-08-19 13:20:03 +0900

    all 4 models implemented

--- model.py
+++ model.py
@@ -7,20 +7,31 @@ class CNN(nn.Module):
     def __init__(self, **kwargs):
         super(CNN, self).__init__()
 
+        self.MODEL = kwargs["MODEL"]
         self.BATCH_SIZE = kwargs["BATCH_SIZE"]
         self.MAX_SENT_LEN = kwargs["MAX_SENT_LEN"]
         self.WORD_DIM = kwargs["WORD_DIM"]
-        self.IN_CHANNEL = kwargs["IN_CHANNEL"]
         self.VOCAB_SIZE = kwargs["VOCAB_SIZE"]
         self.CLASS_SIZE = kwargs["CLASS_SIZE"]
         self.FILTERS = kwargs["FILTERS"]
         self.FILTER_NUM = kwargs["FILTER_NUM"]
         self.DROPOUT_PROB = kwargs["DROPOUT_PROB"]
+        self.IN_CHANNEL = 1
 
         assert (len(self.FILTERS) == len(self.FILTER_NUM))
 
         # one for UNK and one for zero padding
         self.embedding = nn.Embedding(self.VOCAB_SIZE + 2, self.WORD_DIM, padding_idx=self.VOCAB_SIZE + 1)
+        if self.MODEL == "static" or self.MODEL == "non-static" or self.MODEL == "multichannel":
+            self.WV_MATRIX = kwargs["WV_MATRIX"]
+            self.embedding.weight.data.copy_(torch.from_numpy(self.WV_MATRIX))
+            if self.MODEL == "static":
+                self.embedding.weight.requires_grad = False
+            elif self.MODEL == "multichannel":
+                self.embedding2 = nn.Embedding(self.VOCAB_SIZE + 2, self.WORD_DIM, padding_idx=self.VOCAB_SIZE + 1)
+                self.embedding2.weight.data.copy_(torch.from_numpy(self.WV_MATRIX))
+                self.embedding2.weight.requires_grad = False
+                self.IN_CHANNEL = 2
 
         for i in range(len(self.FILTERS)):
             conv = nn.Conv1d(self.IN_CHANNEL, self.FILTER_NUM[i], self.WORD_DIM * self.FILTERS[i], stride=self.WORD_DIM)
@@ -31,9 +42,11 @@ class CNN(nn.Module):
     def get_conv(self, i):
         return getattr(self, f'conv_{i}')
 
-    def forward(self, x, is_train=False):
-        x = self.embedding(x)
-        x = x.view(-1, self.IN_CHANNEL, self.WORD_DIM * self.MAX_SENT_LEN)
+    def forward(self, inp):
+        x = self.embedding(inp).view(-1, 1, self.WORD_DIM * self.MAX_SENT_LEN)
+        if self.MODEL == "multichannel":
+            x2 = self.embedding2(inp).view(-1, 1, self.WORD_DIM * self.MAX_SENT_LEN)
+            x = torch.cat((x, x2), 1)
 
         conv_results = [
             F.max_pool1d(F.relu(self.get_conv(i)(x)), self.MAX_SENT_LEN - self.FILTERS[i] + 1)
@@ -41,7 +54,7 @@ class CNN(nn.Module):
             for i in range(len(self.FILTERS))]
 
         x = torch.cat(conv_results, 1)
-        x = F.dropout(x, p=self.DROPOUT_PROB, training=is_train)
+        x = F.dropout(x, p=self.DROPOUT_PROB, training=self.training)
         x = self.fc(x)
 
         return x
